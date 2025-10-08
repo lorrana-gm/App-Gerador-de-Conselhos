fc60d8fb8276c90ec5683317f2918cde956ff060
Lorrana Martins <lorranamartins961@gmail.com>
2025-10-07 22:19:55 -0300

    arquivo adicionado

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const idDoConselho = document.querySelector('.titulo');
+const descricaoDoConselho = document.querySelector('.descricao')
+
+document.getElementById('botao').addEventListener('click', pegarConselhos)
+
+async function pegarConselhos() {
+    try {
+        const resposta = await fetch('https://api.adviceslip.com/advice')
+        const conteudoDoConselho = await resposta.json()
+        idDoConselho.innerText = "Advice #" + conteudoDoConselho.slip.id
+        descricaoDoConselho.innerText = `"${conteudoDoConselho.slip.advice}"`
+    } catch (err) {
+        console.log("Ocorreu um erro ao tentar buscar as informações da API", err);
+    }
+}
+
+pegarConselhos()
+
+
+
+
+
+
+
